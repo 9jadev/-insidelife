61ac2666a92cd6c5009e544b3e10468eed142379
solomon <ahambasolomon800@gmail.com>
2019-09-02 23:42:55 +0100

    lets check

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="blog-footer">
-  <p> <a href="#">InsideLife</a> by <a href="https://twitter.com/Solomonahamba">@solomonahamba</a>.</p>
+  <p> <a href="#">InsideLife</a> by <a href="https://twitter.com/Solomonahamba">Ahamba solomon</a>.</p>
   <p>
     <a href="#">Back to top</a>
   </p>
